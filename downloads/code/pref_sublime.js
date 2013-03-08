428edb8cc6af66172f6bbb300eb94d67a5e277db
Marc Friederich <marc@antistatique.net>
2013-03-08 22:17:06 +0100

    Site updated at 2013-03-08 21:17:06 UTC

---
+++ downloads/code/pref_sublime.js
@@ -0,0 +1,26 @@
+{
+    "auto_complete": false,
+    "close_windows_when_empty": false,
+    "color_scheme": "Packages/Color Scheme - Default/zuff.tmTheme",
+    "draw_white_space": "all",
+    "find_selected_text": true,
+    "fold_buttons": false,
+    "folder_exclude_patterns":
+    [
+        ".svn",
+        ".git"
+    ],
+    "font_options":
+    [
+        "subpixel_antialias"
+    ],
+    "font_size": 12.0,
+    "highlight_line": true,
+    "ignored_packages":
+    [
+        "Vintage"
+    ],
+    "theme": "Soda Dark.sublime-theme",
+    "translate_tabs_to_spaces": true,
+    "trim_trailing_white_space_on_save": true
+}
